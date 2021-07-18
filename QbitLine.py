72bcac6f407da69a6cce33a1c87c28d24ea7ed1a
aucejito <tomanotademicorreo@hotmail.com>
2021-07-18 20:48:27 +0200

    Retoque GUI

--- QbitLine.py
+++ QbitLine.py
@@ -6,16 +6,23 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
+blank = None
+
 class QbitLine(QFrame):
-    grid = QHBoxLayout()
+    grid = QGridLayout()
+    currColumn = 2
     
     def __init__(self, *args, **kwargs):
         QFrame.__init__(self, *args, **kwargs)
         self.setFrameShape(QtWidgets.QFrame.HLine)
-        global grid
-        grid = QHBoxLayout(self)
-        grid.setContentsMargins(0,0,0,0)
-        #grid.addWidget(QWidget(self).setStyleSheet("background-color: yellow"))
+        self.grid = QGridLayout(self)
+        self.grid.setContentsMargins(0,0,0,0)
+        global blank
+        blank = QLabel()
+        blank.setPixmap(QPixmap('./images/dashedsquare.png'))
+        blank.setMaximumSize(50,50)
+        blank.setScaledContents(True)
+        self.grid.addWidget(blank, 0, 2)
         self.setAcceptDrops(True)
 
     def dragEnterEvent(self, event):
@@ -31,9 +38,16 @@ class QbitLine(QFrame):
             newGate.setScaledContents(True)
             newGate.setMaximumSize(50,50)
             newGate.setAlignment(Qt.AlignCenter)
-            grid.addWidget(newGate)
-
+            blank = QLabel()
+            blank.setAcceptDrops(True)
+            blank.setPixmap(QPixmap('./images/dashedsquare.png'))
+            blank.setMaximumSize(50,50)
+            blank.setScaledContents(True)
+            self.grid.addWidget(newGate, 0, self.currColumn)
+            #self.grid.addWidget(blank, 0, self.currColumn-1)
+            self.grid.addWidget(blank, 0, self.currColumn+1)
+            self.currColumn += 2
             currentCircuit = Circuit()
             currentCircuit.addGate(event.mimeData().data)
-
+        print(self.grid.count())
             
